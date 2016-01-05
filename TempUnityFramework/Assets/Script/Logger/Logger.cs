538a8caa8d9f299448da66b953d825d038c01bd5
lshain@gmail.com <lshan>
2016-01-05 18:19:47 +0800

    update

---
+++ TempUnityFramework/Assets/Script/Logger/Logger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+using System.Collections;
+using System.Diagnostics;
+
+public static class Logger
+{
+	[Conditional("DEBUG")]
+    public static void E(string tag, string msg)
+    {
+		UnityEngine.Debug.LogError(tag + ": " + msg);
+    }
+
+	[Conditional("DEBUG")]
+    public static void V(string tag, string msg)
+    {
+		UnityEngine.Debug.Log(tag + ": " + msg);
+    }
+
+	[Conditional("DEBUG")]
+    public static void W(string tag, string msg)
+    {
+		UnityEngine.Debug.LogWarning(tag + ": " + msg);
+    }
+}
